8357066cf361825f295bf715a0b3d3393e6698bf
troy_schuetrumpf <Troy.Schuetrumpf@gmail.com>
2014-07-19 22:31:02 +1000

    http router and negroni server added

--- server.go
+++ server.go
@@ -7,4 +7,12 @@ import (
 )
 
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "Go Back")
+	})
+
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	n.Run(":4000")
 }
